52558be847e5ab91603e5711809d0e53c76b2861
giric <girichev2013@yandex.ru>
2023-02-27 22:33:57 +0500

    27.02.2023 - Users тесты постмана прошел

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.exception.exceptions.InvalidOwnerException;
 import ru.practicum.shareit.exception.exceptions.ResourceNotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserMapper;
@@ -32,13 +31,7 @@ public class UserServiceImpl implements UserService {
     public UserDto addUser(UserDto userDto) {
         log.info("Получен запрос на добавление пользователя " + userDto.getEmail());
         User user = UserMapper.dtoToUser(userDto);
-        if (!checkEmailDuplicate(user)) {
-            UserMapper.userToDto(userRepository.save(user));
-            Optional<User> us = userRepository.findById(1L);
-            return UserMapper.userToDto(us.get());
-        } else {
-            throw new InvalidOwnerException("Пользователь с Email " + user.getEmail() + " не может быть добавлен");
-        }
+        return UserMapper.userToDto(userRepository.save(user));
     }
 
     @Transactional
@@ -46,16 +39,20 @@ public class UserServiceImpl implements UserService {
     public UserDto updateUser(UserDto userDto, Long userId) {
         log.info("Получен запрос на обновление пользователя " + userId);
         User user = UserMapper.dtoToUser(userDto);
-        List<User> allUsers = userRepository.findAll();
-        if (!allUsers.stream().filter(u -> !u.getId().equals(userId)).map(User::getEmail).anyMatch(e -> e.equals(userDto.getEmail()))) {
-            if (allUsers.stream().anyMatch(u -> u.getId().equals(userId))) {
-                return UserMapper.userToDto(userRepository.save(user));
-            } else {
-                throw new ResourceNotFoundException("Пользователь с ID " + user.getEmail() + " не найден");
+        Optional<User> oldUser = userRepository.findById(userId);
+        if (oldUser.isPresent()) {
+            if (user.getId() == null) {
+                user.setId(userId);
             }
+            if (user.getName() == null) {
+                user.setName(oldUser.get().getName());
+            }
+            if (user.getEmail() == null) {
+                user.setEmail(oldUser.get().getEmail());
+            }
+            return UserMapper.userToDto(userRepository.save(user));
         } else {
-            throw new InvalidOwnerException("Пользователь ID " + userId + " пытался обновить Email " +
-                    user.getEmail() + " данный Email уже занят");
+            throw new ResourceNotFoundException("Пользователь с ID " + user.getEmail() + " не найден");
         }
     }
 
@@ -84,9 +81,5 @@ public class UserServiceImpl implements UserService {
     public List<UserDto> getAllUsers() {
         log.info("Получен запрос на получение списка всех пользователей");
         return userRepository.findAll().stream().map(UserMapper::userToDto).collect(Collectors.toList());
-    }
-
-    private boolean checkEmailDuplicate(User user) {
-        return userRepository.findAll().stream().map(User::getEmail).anyMatch(u -> u.equals(user.getEmail()));
     }
 }
