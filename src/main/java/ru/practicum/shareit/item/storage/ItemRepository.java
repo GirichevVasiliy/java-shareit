b2dfe1128c354dde2297630d830bfaa5ecaeaf3a
giric <girichev2013@yandex.ru>
2023-02-28 19:36:28 +0500

    27.02.2023 - Items и User тесты прошел

--- src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
@@ -9,5 +10,9 @@ import java.util.List;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByOwnerId(Long Id);
-    List<Item> findAllByNameContainingIgnoreCaseAndDescriptionContainingIgnoreCaseAndAvailableIsTrue(String textName, String textDescription);
+    @Query("select i from Item i " +
+            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
+            "   or upper(i.description) like upper(concat('%', ?1, '%')) and i.available = true")
+    List<Item> getAvailableItems(String text);
+   // List<Item> findAllByNameContainingIgnoreCaseAndDescriptionContainingIgnoreCaseAndAvailableIsTrue(String textName, String textDescription);
 }
