7fa03c30c224f20bdefa0075a4699e7c57b287ab
giric <girichev2013@yandex.ru>
2023-03-04 14:40:12 +0500

    04.03.2023 -  rev1 - code stylе ver2

--- src/main/java/ru/practicum/shareit/exception/exceptions/ValidationStateException.java
+++ src/main/java/ru/practicum/shareit/exception/exceptions/ValidationStateException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception.exceptions;
 
-public class ValidationStateException extends RuntimeException{
+public class ValidationStateException extends RuntimeException {
     public ValidationStateException(String message) {
         super(message);
     }
