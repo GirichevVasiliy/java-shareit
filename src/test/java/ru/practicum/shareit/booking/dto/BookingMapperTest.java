a6be90cffbb0455bf6f0b162fbb8fcfdfc5d05ab
giric <girichev2013@yandex.ru>
2023-03-18 13:25:52 +0500

    17.03.2023 - test UserControllerTest

---
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingMapperTest.java
@@ -0,0 +1,201 @@
+package ru.practicum.shareit.booking.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.StatusBooking;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BookingMapperTest {
+    private BookingDto bookingDtoControl;
+    private InputBookingDto inputBookingDto;
+    private Booking booking;
+    private User owner;
+    private ItemDto itemDto;
+    private UserDto ownerDto;
+   private ItemRequest itemRequest;
+   private Item item;
+   private DateBookingDto dateBookingDto;
+    @BeforeEach
+    private void init() {
+        owner = User.builder()
+                .id(1L)
+                .name("user1")
+                .email("y1@email.ru")
+                .build();
+        ownerDto = UserDto.builder()
+                .id(1L)
+                .name("user1")
+                .email("y1@email.ru")
+                .build();
+        itemRequest = ItemRequest.builder()
+                .id(1L)
+                .description("text")
+                .requestor(owner)
+                .created(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .build();
+        itemDto = ItemDto.builder()
+                .id(1L)
+                .name("item1")
+                .description("text")
+                .available(true)
+                .owner(ownerDto)
+                .requestId(1L)
+                .build();
+        inputBookingDto = InputBookingDto.builder()
+                .start(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .end(LocalDateTime.parse("2024-10-23T17:19:45"))
+                .build();
+        booking = Booking.builder()
+                .id(1L)
+                .start(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .end(LocalDateTime.parse("2024-10-23T17:19:45"))
+                .item(new Item(1L, "item1", "text", true, owner, itemRequest,new ArrayList<>()))
+                .booker(owner)
+                .status(StatusBooking.APPROVED)
+                .build();
+        bookingDtoControl = BookingDto.builder()
+                .id(1L)
+                .start(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .end(LocalDateTime.parse("2024-10-23T17:19:45"))
+                .item(itemDto)
+                .booker(ownerDto)
+                .status(StatusBooking.APPROVED.name())
+                .build();
+        dateBookingDto = DateBookingDto.builder()
+                .id(1L)
+                .bookerId(1L)
+                .start(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .end(LocalDateTime.parse("2024-10-23T17:19:45"))
+                .build();
+        item = Item.builder()
+                .id(1L)
+                .name("item1")
+                .description("text")
+                .available(true)
+                .owner(owner)
+                .comments(new ArrayList<>())
+                .request(itemRequest)
+                .build();
+    }
+
+    @Test
+    @DisplayName("Стандартный тест перевода booking в bookingDto")
+    void bookingToDtoTest() {
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(bookingDtoControl.getId())).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(bookingDtoControl.getStart())).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(bookingDtoControl.getEnd())).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(bookingDtoControl.getBooker())).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(bookingDtoControl.getItem())).isTrue();
+    }
+    @Test
+    @DisplayName("Tест проверки обновления id, при переводе booking в bookingDto")
+    void bookingToDtoTestNewId() {
+        final Long newId = 33L;
+        booking.setId(newId);
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(newId)).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(bookingDtoControl.getStart())).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(bookingDtoControl.getEnd())).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(bookingDtoControl.getBooker())).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(bookingDtoControl.getItem())).isTrue();
+    }
+    @Test
+    @DisplayName("Tест проверки обновления StartTime, при переводе booking в bookingDto")
+    void bookingToDtoTestNewStartTime() {
+        final LocalDateTime newTime = LocalDateTime.parse("2045-10-23T19:19:00");
+        booking.setStart(LocalDateTime.parse("2045-10-23T19:19:00"));
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(bookingDtoControl.getId())).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(newTime)).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(bookingDtoControl.getEnd())).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(bookingDtoControl.getBooker())).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(bookingDtoControl.getItem())).isTrue();
+    }
+    @Test
+    @DisplayName("Tест проверки обновления EndTime, при переводе booking в bookingDto")
+    void bookingToDtoTestNewEndTime() {
+        final LocalDateTime newTime = LocalDateTime.parse("2045-10-23T19:19:00");
+        booking.setEnd(newTime);
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(bookingDtoControl.getId())).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(bookingDtoControl.getStart())).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(newTime)).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(bookingDtoControl.getBooker())).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(bookingDtoControl.getItem())).isTrue();
+    }
+    @Test
+    @DisplayName("Tест проверки обновления User, при переводе booking в bookingDto")
+    void bookingToDtoTestNewBooker() {
+        User newUser = User.builder()
+                .id(2L)
+                .name("user555")
+                .email("y555@email.ru")
+                .build();
+        UserDto newUserDto = UserDto.builder()
+                .id(2L)
+                .name("user555")
+                .email("y555@email.ru")
+                .build();
+        booking.setBooker(newUser);
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(bookingDtoControl.getId())).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(bookingDtoControl.getStart())).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(bookingDtoControl.getEnd())).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(newUserDto)).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(bookingDtoControl.getItem())).isTrue();
+    }
+    @Test
+    @DisplayName("Tест проверки обновления Item, при переводе booking в bookingDto")
+    void bookingToDtoTestNewItem() {
+        booking = Booking.builder()
+                .id(1L)
+                .start(LocalDateTime.parse("2024-10-23T17:19:33"))
+                .end(LocalDateTime.parse("2024-10-23T17:19:45"))
+                .item(new Item(2L, "item2", "text2", false, owner, itemRequest,new ArrayList<>()))
+                .booker(owner)
+                .status(StatusBooking.APPROVED)
+                .build();
+        itemDto = ItemDto.builder()
+                .id(2L)
+                .name("item2")
+                .description("text2")
+                .available(false)
+                .owner(ownerDto)
+                .requestId(1L)
+                .build();
+        BookingDto bookingDtoNew = BookingMapper.bookingToDto(booking);
+        assertThat(bookingDtoNew.getId().equals(bookingDtoControl.getId())).isTrue();
+        assertThat(bookingDtoNew.getStart().equals(bookingDtoControl.getStart())).isTrue();
+        assertThat(bookingDtoNew.getEnd().equals(bookingDtoControl.getEnd())).isTrue();
+        assertThat(bookingDtoNew.getBooker().equals(bookingDtoControl.getBooker())).isTrue();
+        assertThat(bookingDtoNew.getItem().equals(itemDto)).isTrue();
+    }
+    @Test
+    @DisplayName("Стандартный тест создани из bookingDto booking")
+    void createNewBookingStandardTest() {
+        Booking newBooking = BookingMapper.createNewBooking(inputBookingDto, owner, item);
+        assertThat(newBooking.getStart().equals(booking.getStart())).isTrue();
+        assertThat(newBooking.getEnd().equals(booking.getEnd())).isTrue();
+        assertThat(newBooking.getBooker().equals(booking.getBooker())).isTrue();
+        assertThat(newBooking.getItem().equals(booking.getItem())).isTrue();
+        assertThat(newBooking.getStatus().equals(StatusBooking.WAITING)).isTrue();
+    }
+
+    @Test
+    void toDateBookingDto() {
+    }
+}
--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -10,16 +10,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.exception.ResourceNotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
-import static org.apache.tomcat.jni.File.putc;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -69,6 +71,7 @@ class UserControllerTest {
         verify(userService, times(1)).addUser(any());
         assertEquals(objectMapper.writeValueAsString(userDto), result);
     }
+
     @Test
     @SneakyThrows
     void addUserTest_whenUserDtoNameNotValid_thenClientError() {
@@ -78,11 +81,22 @@ class UserControllerTest {
                 .andExpect(status().is4xxClientError());
         verify(userService, never()).addUser(any());
     }
+
     @Test
     @SneakyThrows
     void addUserTest_whenUserDtoEmailNotValid_thenClientError() {
         mockMvc.perform(post("/users")
                         .content(objectMapper.writeValueAsString(userDtoEmail))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+        verify(userService, never()).addUser(any());
+    }
+
+    @Test
+    @SneakyThrows
+    void addUserTest_whenUserDtoNotValid_thenClientError() {
+        mockMvc.perform(post("/users")
+                        .content(objectMapper.writeValueAsString(null))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError());
         verify(userService, never()).addUser(any());
@@ -102,30 +116,141 @@ class UserControllerTest {
         verify(userService, times(1)).updateUser(userDto, userId);
         assertEquals(objectMapper.writeValueAsString(userDto), result);
     }
+
     @Test
     @SneakyThrows
-    void updateUserTest_when_thenReturnedOk() {
-            when(userService.updateUser(userDtoEmail, userId)).thenReturn(userDto);
-            String result = mockMvc.perform(patch("/users/{id}", userId)
-                            .content(objectMapper.writeValueAsString(userDtoEmail))
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().is2xxSuccessful())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
-            verify(userService, times(1)).updateUser(userDtoEmail, userId);
-            assertEquals(objectMapper.writeValueAsString(userDto), result);
+    void updateUserTest_whenUserDtoEmailEmpty_thenReturnedOk() {
+        when(userService.updateUser(userDtoEmail, userId)).thenReturn(userDto);
+        String result = mockMvc.perform(patch("/users/{id}", userId)
+                        .content(objectMapper.writeValueAsString(userDtoEmail))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        verify(userService, times(1)).updateUser(userDtoEmail, userId);
+        assertEquals(objectMapper.writeValueAsString(userDto), result);
     }
 
     @Test
-    void getUsers() {
+    @SneakyThrows
+    void updateUserTest_whenUserDtoNameEmpty_thenReturnedOk() {
+        when(userService.updateUser(userDtoName, userId)).thenReturn(userDto);
+        String result = mockMvc.perform(patch("/users/{id}", userId)
+                        .content(objectMapper.writeValueAsString(userDtoName))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        verify(userService, times(1)).updateUser(userDtoName, userId);
+        assertEquals(objectMapper.writeValueAsString(userDto), result);
     }
 
     @Test
-    void getUserById() {
+    @SneakyThrows
+    void updateUserTest_whenUserDtoNotValid_thenClientError() {
+        mockMvc.perform(patch("/users/{id}", userId)
+                        .content(objectMapper.writeValueAsString(null))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+        verify(userService, never()).updateUser(any(), any());
     }
 
     @Test
-    void deleteUser() {
+    @SneakyThrows
+    void updateUserTest_whenUserIdNotValid_thenClientError() {
+        mockMvc.perform(patch("/users/{id}", "")
+                        .content(objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+        verify(userService, never()).updateUser(any(), any());
+    }
+
+    @Test
+    @SneakyThrows
+    void updateUserTest_whenUserNotFound_thenClientError() {
+        when(userService.getUserById(userId)).thenThrow(
+                new ResourceNotFoundException("Пользователь с ID " + userId + " не найден"));
+        mockMvc.perform(patch("/users/{id}", userId))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @SneakyThrows
+    void getUsers_whenUserDtoNameIsNotEmpty_thenReturnedOk() {
+        List<UserDto> userDtoList = Arrays.asList(userDto);
+        when(userService.getAllUsers()).thenReturn(userDtoList);
+        String result = mockMvc.perform(get("/users")
+                        .content(objectMapper.writeValueAsString(userDtoList))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        verify(userService, times(1)).getAllUsers();
+        assertEquals(objectMapper.writeValueAsString(userDtoList), result);
+    }
+
+    @Test
+    @SneakyThrows
+    void getUsers_whenUserDtoListIsEmpty_thenReturnedOk() {
+        List<UserDto> userDtoList = new ArrayList<>();
+        when(userService.getAllUsers()).thenReturn(userDtoList);
+        String result = mockMvc.perform(get("/users")
+                        .content(objectMapper.writeValueAsString(userDtoList))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        verify(userService, times(1)).getAllUsers();
+        assertEquals(objectMapper.writeValueAsString(userDtoList), result);
+    }
+
+    @Test
+    @SneakyThrows
+    void getUserById_whenUserIdValid_thenReturnedOk() {
+        when(userService.getUserById(userId)).thenReturn(userDto);
+        String result = mockMvc.perform(get("/users/{id}", userId)
+                        .content(objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        verify(userService, times(1)).getUserById(userId);
+        assertEquals(objectMapper.writeValueAsString(userDto), result);
+    }
+
+    @Test
+    @SneakyThrows
+    void getUserById_whenUserIdNotValid_thenClientError() {
+        mockMvc.perform(get("/users/{id}", "q"))
+                .andExpect(status().is4xxClientError());
+        verify(userService, never()).getUserById(userId);
+    }
+
+    @Test
+    @SneakyThrows
+    void getUserById_whenUserNotFound_thenClientError() {
+        when(userService.getUserById(userId)).thenThrow(
+                new ResourceNotFoundException("Пользователь с ID " + userId + " не найден"));
+        mockMvc.perform(get("/users/{id}", userId))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @SneakyThrows
+    void deleteUser_whenUserNotFound_thenClientError() {
+        mockMvc.perform(delete("/users/{id}", userId))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @SneakyThrows
+    void deleteUser_whenUserIdNotValid_thenClientError() {
+        mockMvc.perform(delete("/users/{id}", "e"))
+                .andExpect(status().is4xxClientError());
     }
 }
