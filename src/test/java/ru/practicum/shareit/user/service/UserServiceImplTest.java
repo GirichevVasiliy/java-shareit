3809e81b8f78041e33308902e4bfa1ae835a66ca
giric <girichev2013@yandex.ru>
2023-03-19 23:00:12 +0500

    19.03.2023 - test UserServiceImplTest

--- src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -7,9 +8,19 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import ru.practicum.shareit.exception.ForbiddenResourceException;
+import ru.practicum.shareit.exception.ResourceNotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserRepository;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class UserServiceImplTest {
@@ -17,14 +28,44 @@ class UserServiceImplTest {
     UserServiceImpl userService;
     @Mock
     UserRepository userRepository;
-    @Test
-    void addUser() {
+    private User user;
+    private UserDto userDto;
+    final Long userId1 = 1L;
+    @BeforeEach
+    private void init() {
+        user = User.builder()
+                .id(1L)
+                .name("user1")
+                .email("y1@email.ru")
+                .build();
+        userDto = UserDto.builder()
+                .id(1L)
+                .name("user1")
+                .email("y1@email.ru")
+                .build();
     }
-
     @Test
-    void updateUser() {
+    void addUser_whenСorrectUser_thenReturnUserDto() {
+        when(userRepository.save(user)).thenReturn(user);
+        UserDto newUserDto = userService.addUser(userDto);
+        verify(userRepository, times(1)).save(any());
+        assertThat(newUserDto.equals(userDto)).isTrue();
     }
-
+    @Test
+    void updateUser_whenUserIsNotOwnerItem_thenThrowException() {
+        assertThrows(
+                ResourceNotFoundException.class,
+                () -> userService.updateUser(userDto,userId1));
+        verify(userRepository, never()).save(any());
+    }
+    @Test
+    void updateUser_whenСorrectUser_thenReturnUserDto() {
+        when(userRepository.findById(userId1)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(user);
+        UserDto newUserDto = userService.updateUser(userDto, userId1);
+        verify(userRepository, times(1)).save(any());
+        assertThat(newUserDto.equals(userDto)).isTrue();
+    }
     @Test
     void getUserById() {
     }
