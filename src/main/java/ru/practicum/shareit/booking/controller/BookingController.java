a7c7ea967dac910c2879e53387fdcd35b2c7f971
giric <girichev2013@yandex.ru>
2023-03-21 22:42:24 +0500

    21.03.2023 - UserDtoTest

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.booking;
+package ru.practicum.shareit.booking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
