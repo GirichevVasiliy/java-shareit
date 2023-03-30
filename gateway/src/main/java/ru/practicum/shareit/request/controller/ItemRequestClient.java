8951abde9ff5238f4a7f882b156173c8cab22be1
giric <girichev2013@yandex.ru>
2023-03-31 00:00:04 +0500

    30.03.23 - gateway - запущен.

--- gateway/src/main/java/ru/practicum/shareit/request/controller/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/controller/ItemRequestClient.java
@@ -17,7 +17,7 @@ public class ItemRequestClient extends BaseClient {
     private static final String API_PREFIX = "/requests";
 
     @Autowired
-    public ItemRequestClient(@Value("${share-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public ItemRequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
--- gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -14,7 +14,7 @@ public class UserClient extends BaseClient {
     private static final String API_PREFIX = "/users";
 
     @Autowired
-    public UserClient(@Value("${share-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
